d6f174e976c5b9744fa9d2bc78a07404e82e06a8
Colin Shaw <colin.shaw2@uoit.net>
2021-04-21 18:36:50 -0400

    Adder a wrapper class to make other components draggable

---
+++ screen-of-many-things/src/components/test-component.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { DraggableWrapper } from "./HOC/DraggableWrapper";
+
+interface testProps {
+  inputText?: string
+}
+
+export const TestComponent: React.FunctionComponent<testProps> = (props) => {
+  return (
+      <DraggableWrapper>
+          <p>Some Text and stuff and {props.inputText || "no props entered this is the default"}</p>
+    </DraggableWrapper>
+  )
+};
